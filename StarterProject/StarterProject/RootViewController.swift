72ad692066ed8a111adec0d8c801ccfb5620eca7
Tim <tim@designexe.de>
2021-11-11 23:29:05 +0100

    init

---
+++ StarterProject/StarterProject/RootViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RootViewController.swift
+//  StarterProject
+//
+//  Created by Tim Irrgang on 11.11.21.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
